92bd605ff382a7c53a6b069daced3f7ac817d69e
GalalMohammed <galalmoh2001@gmail.com>
2024-01-06 00:46:40 +0200

    add task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script provides some stats about Nginx logs stored in MongoDB.
+
+Example:
+    ./12-log_stats.py
+
+"""
+
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    nginx = client.logs.nginx
+    print(nginx.count_documents({}), "logs")
+    print("Methods:")
+    print("\tmethod GET:", nginx.count_documents({"method": "GET"}))
+    print("\tmethod POST:", nginx.count_documents({"method": "POST"}))
+    print("\tmethod PUT:", nginx.count_documents({"method": "PUT"}))
+    print("\tmethod PATCH:", nginx.count_documents({"method": "PATCH"}))
+    print("\tmethod DELETE:", nginx.count_documents({"method": "DELETE"}))
+    print(nginx.count_documents({"method": "GET", "path": "/status"}),
+          "status check")
