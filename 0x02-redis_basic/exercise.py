e05051a5a4e182bc9acfa92b9627eea1293f627d
GalalMohammed <galalmoh2001@gmail.com>
2024-01-07 22:48:50 +0200

    add task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,9 +10,41 @@ Example:
 
 
 import uuid
+from functools import wraps
 from typing import Union, Optional
 from collections.abc import Callable
 import redis
+
+
+def count_calls(method: Callable) -> Callable:
+    """count how many times methods of the Cache class are called.
+
+    Args:
+        method (object): Cache method.
+
+    Returns:
+        incrementing wrapper.
+
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """Count how many times methods of the Cache class are called.
+
+        Args:
+            self: instance.
+            *args: Variable length argument list.
+            **kwds: keyword arguments.
+
+        Returns:
+            value returned by original method.
+
+        """
+        if self._redis.exists(method.__qualname__):
+            self._redis.incr(method.__qualname__)
+        else:
+            self._redis.set(method.__qualname__, 1)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
@@ -23,6 +55,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a random key.
 
@@ -37,6 +70,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
+    @count_calls
     def get(self, key: str,
             fn:Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """Retrieve from the server.
@@ -54,6 +88,7 @@ class Cache:
             return fn(data)
         return data
 
+    @count_calls
     def get_str(self, key: str) -> str:
         """Retrieve from the server.
 
@@ -66,6 +101,7 @@ class Cache:
         """
         return str(self._redis.get(key))
     
+    @count_calls
     def get_int(self, key: str) -> int:
         """Retrieve from the server.
 
