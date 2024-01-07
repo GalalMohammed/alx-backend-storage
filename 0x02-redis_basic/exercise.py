52f0298273ec1244ac745953288e4ae8759ff869
GalalMohammed <galalmoh2001@gmail.com>
2024-01-07 21:39:16 +0200

    order matters to the checker

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,7 +22,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis using a random key.
 
         Args:
