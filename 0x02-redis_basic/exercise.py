ef35341063f383f693ef239abbcdcc46ea4e2f7e
GalalMohammed <galalmoh2001@gmail.com>
2024-01-07 21:22:24 +0200

    add task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module defines a Cache class.
+
+Example:
+    cache = Cache()
+
+"""
+
+
+import uuid
+import redis
+
+
+class Cache:
+    """Cache class."""
+
+    def __init__(self) -> None:
+        """__init__ method."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes | str | int | float) -> str:
+        """Store the input data in Redis using a random key.
+
+        Args:
+            data (object): input data.
+
+        Returns:
+            the key.
+
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
